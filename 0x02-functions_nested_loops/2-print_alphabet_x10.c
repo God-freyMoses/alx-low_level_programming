2c2a40dbdda4c8557faf605d4a8768e0e8e9cc47
God-freyMoses <godfreymoses203@gmail.com>
2023-06-20 13:55:45 -0700

    Print alphabets in 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
