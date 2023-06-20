66c5183bfe543edbc0d792ab5d3fa3854481f65e
God-freyMoses <godfreymoses203@gmail.com>
2023-06-20 14:16:57 -0700

    Print 10 lines of alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,25 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
+ *
  */
+
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	char ch;
+	int i;
 
-	for (i = 0; i < 10; i++)
-	   print_alphabet();
+	i = 0;
 
-	_putchar('\n');
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
