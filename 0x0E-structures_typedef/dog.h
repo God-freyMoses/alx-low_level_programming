c158f0b118d916ae86905950e9c4943a3fa2a33d
God-freyMoses <godfreymoses203@gmail.com>
2023-07-17 18:29:10 -0700

    First one out of the way

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - initializes
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: dog ownwer
+ *
+ * Description: Well it is dog struct,thats all.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
